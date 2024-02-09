158f41add7d98c2bcc16a8079cf6e67e40b32714
vitodev-r <speedysafeshop@gmail.com>
2024-02-09 17:37:45 -0300

    config proyect

---
+++ Dev-apis/apigym/src/main/java/com/example/apigym/ApigymApplication.java
@@ -0,0 +1,13 @@
+package com.example.apigym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigymApplication.class, args);
+	}
+
+}
---
+++ Dev-apis/apigym/src/test/java/com/example/apigym/ApigymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apigym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
